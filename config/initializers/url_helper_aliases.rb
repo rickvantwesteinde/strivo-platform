1a0db1a657645601929a451615288664f6b9fd3b
rickvantwesteinde <rick@vantwestein.de>
2025-10-26 22:47:56 +0100

    Update url_helper_aliases.rb

--- config/initializers/url_helper_aliases.rb
+++ config/initializers/url_helper_aliases.rb
@@ -1,29 +1,24 @@
 # config/initializers/url_helper_aliases.rb
 # Zorg dat spree_login_path overal beschikbaar is (controllers, views, specs)
+
 Rails.application.config.to_prepare do
   app_helpers   = Rails.application.routes.url_helpers
-  spree_helpers = Spree::Core::Engine.routes.url_helpers
 
-  # Definieer spree_login_path als hij niet bestaat
+  # Definieer spree_login_path één keer op de app URL helpers
   unless app_helpers.method_defined?(:spree_login_path)
     app_helpers.module_eval do
       def spree_login_path
-        helpers = Spree::Core::Engine.routes.url_helpers
-
-        if helpers.respond_to?(:spree_login_path)
-          helpers.spree_login_path
-        elsif helpers.respond_to?(:new_spree_user_session_path)
-          helpers.new_spree_user_session_path
-        elsif helpers.respond_to?(:login_path)
-          helpers.login_path
+        # Prefer Spree's eigen /login als die route bestaat, anders val terug op Devise
+        if Spree::Core::Engine.routes.url_helpers.respond_to?(:login_path)
+          Spree::Core::Engine.routes.url_helpers.login_path
         else
-          '/login'
+          # Dit is wat er in CI daadwerkelijk gebruikt wordt
+          '/user/sign_in'
         end
       end
     end
   end
 
-  # <<-- Dit is de extra stap die nog miste
-  # Voeg ook toe aan ApplicationController, zodat alle controllers/views 'm kennen
-  ApplicationController.helper_method :spree_login_path if ApplicationController.method_defined?(:helper_method)
+  # Maak 'm beschikbaar in controllers en views
+  ApplicationController.helper_method :spree_login_path if ApplicationController.respond_to?(:helper_method)
 end
