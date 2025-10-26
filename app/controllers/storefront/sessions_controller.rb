39dd7ee6de59eddd34ebb48fdee48574a9c6a2ef
rickvantwesteinde <rick@vantwestein.de>
2025-10-26 11:13:20 +0100

    Update sessions_controller.rb

--- app/controllers/storefront/sessions_controller.rb
+++ app/controllers/storefront/sessions_controller.rb
@@ -1,10 +1,17 @@
+# frozen_string_literal: true
+
 module Storefront
   class SessionsController < BaseController
+    def index
+      @class_types = ClassType.all
+      @sessions = Session.includes(:class_type, :trainer)
+                         .where('starts_at >= ?', Time.zone.now.beginning_of_day)
+                         .order(:starts_at)
+    end
+
     def show
-      @session = Session.includes(:class_type, :gym, bookings: :user).find(params[:id])
-      @booking = @session.bookings.confirmed.find_by(user: current_spree_user)
-      @occupancy = @session.spots_taken
-      @spots_left = @session.spots_left
+      @session = Session.find(params[:id])
+      @booking = current_spree_user.bookings.find_by(session: @session)
     end
   end
 end
