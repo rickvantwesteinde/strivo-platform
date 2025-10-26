602d0755a97d90a15ea567b00bdafbe63fe46194
rickvantwesteinde <rick@vantwestein.de>
2025-10-26 11:57:16 +0100

    Update base_controller.rb

--- app/controllers/storefront/base_controller.rb
+++ app/controllers/storefront/base_controller.rb
@@ -2,12 +2,7 @@
 
 module Storefront
   class BaseController < ApplicationController
-    # nodig voor authenticate_spree_user!
-    include Devise::Controllers::Helpers
-
-    # Spree helpers (spree_current_user e.d.)
     include Spree::Core::ControllerHelpers::Auth
-
     include Storefront::CreditsHelper
 
     before_action :authenticate_spree_user!
