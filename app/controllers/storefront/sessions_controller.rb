20681c2c4e2a8f4793a6720e566c8ed76b54b7da
rickvantwesteinde <rick@vantwestein.de>
2025-10-26 11:57:32 +0100

    Update sessions_controller.rb

--- app/controllers/storefront/sessions_controller.rb
+++ app/controllers/storefront/sessions_controller.rb
@@ -2,16 +2,8 @@
 
 module Storefront
   class SessionsController < BaseController
-    def index
-      @class_types = ClassType.all
-      @sessions = Session.includes(:class_type, :trainer)
-                         .where('starts_at >= ?', Time.zone.now.beginning_of_day)
-                         .order(:starts_at)
-    end
-
     def show
       @session = Session.find(params[:id])
-      @booking = current_spree_user.bookings.find_by(session: @session)
     end
   end
 end
