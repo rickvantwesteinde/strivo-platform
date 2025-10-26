b84bc68f311f38bed987ac4bd9f2e6491ea0dc00
rickvantwesteinde <rick@vantwestein.de>
2025-10-26 12:10:23 +0100

    Update base_controller.rb

--- app/controllers/storefront/base_controller.rb
+++ app/controllers/storefront/base_controller.rb
@@ -2,15 +2,29 @@
 
 module Storefront
   class BaseController < ApplicationController
-    include Spree::Core::ControllerHelpers::Auth
+    # Zorg dat Devise helpers zoals current_spree_user beschikbaar zijn
+    include Devise::Controllers::Helpers
+
+    # Credits helper zoals je al had
     include Storefront::CreditsHelper
 
+    # ✅ Shim: bied altijd authenticate_spree_user! aan
+    # - Als je ingelogd bent als spree_user -> OK
+    # - Anders redirecten we naar de loginpagina
     before_action :authenticate_spree_user!
 
     helper Storefront::CreditsHelper
     helper_method :default_gym
 
     private
+
+    def authenticate_spree_user!
+      return if respond_to?(:current_spree_user, true) && current_spree_user.present?
+
+      # simpele, compatibele redirect naar Devise/Spree login
+      # In Spree is dit doorgaans /login
+      redirect_to '/login'
+    end
 
     def default_gym
       @default_gym ||= Gym.order(:id).first
