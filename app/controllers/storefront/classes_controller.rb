2628d60dd2443b395a8a4260d7cc47c33722d522
rickvantwesteinde <rick@vantwestein.de>
2025-10-26 13:26:04 +0100

    Update classes_controller.rb

--- app/controllers/storefront/classes_controller.rb
+++ app/controllers/storefront/classes_controller.rb
@@ -3,7 +3,10 @@
 module Storefront
   class ClassesController < BaseController
     def index
-      @class_types = ClassType.includes(:sessions).order(:name)
+      @class_types = ClassType.order(:name)
+      # Komende sessies groeperen per class_type voor de view
+      upcoming_sessions = Session.where('starts_at >= ?', Time.current).order(:starts_at)
+      @sessions_by_class_type = upcoming_sessions.group_by(&:class_type_id)
     end
   end
 end
