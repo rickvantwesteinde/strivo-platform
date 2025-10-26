9bcf85abccd0a166e9d649d9255a139729bea3f9
rickvantwesteinde <rick@vantwestein.de>
2025-10-26 11:58:16 +0100

    Update classes_controller.rb

--- app/controllers/storefront/classes_controller.rb
+++ app/controllers/storefront/classes_controller.rb
@@ -3,14 +3,7 @@
 module Storefront
   class ClassesController < BaseController
     def index
-      @class_types = ClassType.all
-    end
-
-    def show
-      @class_type = ClassType.find(params[:id])
-      @sessions = @class_type.sessions
-                             .where('starts_at >= ?', Time.zone.now.beginning_of_day)
-                             .order(:starts_at)
+      @class_types = ClassType.includes(:sessions).order(:name)
     end
   end
 end
