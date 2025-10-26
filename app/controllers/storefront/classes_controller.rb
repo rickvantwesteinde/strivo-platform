a8f8f9d49ae6914389f080f8e2802c6eadef18f2
rickvantwesteinde <rick@vantwestein.de>
2025-10-26 11:13:34 +0100

    Update classes_controller.rb

--- app/controllers/storefront/classes_controller.rb
+++ app/controllers/storefront/classes_controller.rb
@@ -1,14 +1,16 @@
+# frozen_string_literal: true
+
 module Storefront
   class ClassesController < BaseController
-    UPCOMING_WINDOW_DAYS = 14
-    SESSIONS_PER_TYPE = 5
-
     def index
-      range_end = UPCOMING_WINDOW_DAYS.days.from_now.end_of_day
-      sessions = Session.includes(:class_type, :gym).upcoming(until_time: range_end)
-      @class_types = ClassType.includes(:gym).order(:name)
-      grouped = sessions.group_by(&:class_type_id)
-      @sessions_by_class_type = grouped.transform_values { |items| items.first(SESSIONS_PER_TYPE) }
+      @class_types = ClassType.all
+    end
+
+    def show
+      @class_type = ClassType.find(params[:id])
+      @sessions = @class_type.sessions
+                             .where('starts_at >= ?', Time.zone.now.beginning_of_day)
+                             .order(:starts_at)
     end
   end
 end
