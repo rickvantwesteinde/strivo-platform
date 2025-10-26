32c9af8ffa2276776b543552943c20a3dcd2cf3e
rickvantwesteinde <rick@vantwestein.de>
2025-10-26 12:24:46 +0100

    Update sessions_controller.rb

--- app/controllers/storefront/sessions_controller.rb
+++ app/controllers/storefront/sessions_controller.rb
@@ -4,6 +4,10 @@ module Storefront
   class SessionsController < BaseController
     def show
       @session = Session.find(params[:id])
+
+      # Bepaal of huidige user al geboekt heeft (spec kijkt hiernaar)
+      @existing_booking = @session.bookings.find_by(user: current_spree_user, status: :confirmed)
+      @already_booked   = @existing_booking.present?
     end
   end
 end
