a6018623cc88df23d8088adc620d82047f817da8
rickvantwesteinde <rick@vantwestein.de>
2025-10-28 20:53:25 +0100

    Update 20250731105000_create_bookings.rb

--- db/migrate/20250731105000_create_bookings.rb
+++ db/migrate/20250731105000_create_bookings.rb
@@ -1,14 +1,18 @@
+# frozen_string_literal: true
+
 class CreateBookings < ActiveRecord::Migration[8.0]
   def change
-    create_table :bookings do |t|
+    # Skip if the table exists
+    return if table_exists?(:bookings)
+
+    create_table :bookings, if_not_exists: true do |t|
       t.references :user, null: false, foreign_key: { to_table: :spree_users }
       t.references :session, null: false, foreign_key: true
       t.integer :status, null: false, default: 0
       t.datetime :canceled_at
-
       t.timestamps
     end
 
-    add_index :bookings, [:user_id, :session_id], unique: true
+    add_index :bookings, [:user_id, :session_id], unique: true unless index_exists?(:bookings, [:user_id, :session_id], unique: true)
   end
 end
