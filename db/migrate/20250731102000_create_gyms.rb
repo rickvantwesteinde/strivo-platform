a32157c5ba6eb9037c5143c26471657cc4531c85
rickvantwesteinde <rick@vantwestein.de>
2025-10-30 01:02:36 +0100

    Update 20250731102000_create_gyms.rb

--- db/migrate/20250731102000_create_gyms.rb
+++ db/migrate/20250731102000_create_gyms.rb
@@ -1,13 +1,18 @@
+# frozen_string_literal: true
+
 class CreateGyms < ActiveRecord::Migration[8.0]
   def change
-    create_table :gyms do |t|
+    # Skip this migration if the table already exists
+    return if table_exists?(:gyms)
+
+    create_table :gyms, if_not_exists: true do |t|
       t.string :name, null: false
       t.string :slug, null: false
       t.string :address
-
       t.timestamps
     end
 
-    add_index :gyms, :slug, unique: true
+    # Avoid duplicating the index
+    add_index :gyms, :slug, unique: true unless index_exists?(:gyms, :slug, unique: true)
   end
 end
