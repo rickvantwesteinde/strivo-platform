69e0d46b0b39c52e4bb8546506bd0d391537d876
rickvantwesteinde <rick@vantwestein.de>
2025-10-31 11:52:49 +0100

    fix(factories): simplify trainer factory with gym and user

--- spec/factories/trainers.rb
+++ spec/factories/trainers.rb
@@ -1,6 +1,11 @@
+# spec/factories/trainers.rb
 FactoryBot.define do
   factory :trainer do
-    association :gym
-    association :user, factory: :spree_user
+    gym # ← korter en werkt hetzelfde
+    user factory: :spree_user # ← of: user { create(:spree_user) }
+
+    trait :with_bio do
+      bio { "Certified yoga instructor with 5 years experience" }
+    end
   end
 end
