9c3a8168706c9a9cdc7ebbc88650cf6b7c21dae9
rickvantwesteinde <rick@vantwestein.de>
2025-10-31 11:55:58 +0100

    fix(factories): fix subscription factory with correct association names

--- spec/factories/subscriptions.rb
+++ spec/factories/subscriptions.rb
@@ -1,9 +1,19 @@
+# spec/factories/subscriptions.rb
 FactoryBot.define do
   factory :subscription do
     association :user, factory: :spree_user
-    association :plan, factory: :subscription_plan
-    gym { plan.gym }
+    association :subscription_plan  # ← correcte factory naam
+    gym { subscription_plan.gym }   # ← gebruik de association
     starts_on { Date.current.beginning_of_month }
     status { :active }
+
+    trait :canceled do
+      status { :canceled }
+      ended_at { 1.day.ago }
+    end
+
+    trait :next_month do
+      starts_on { 1.month.from_now.beginning_of_month }
+    end
   end
 end
