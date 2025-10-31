56b4c3f9d406d4aee326a46104251521538c56dc
rickvantwesteinde <rick@vantwestein.de>
2025-10-31 11:56:34 +0100

    subscription_plan { create(:subscription_plan, gym: gym) } # Niet nodig!

--- spec/factories/bookings.rb
+++ spec/factories/bookings.rb
@@ -1,10 +1,28 @@
+# spec/factories/bookings.rb
 FactoryBot.define do
   factory :booking do
     association :session
-    gym { session.gym }
     association :user, factory: :spree_user
-    subscription_plan { create(:subscription_plan, gym: gym) }
+    association :subscription_plan
+
+    # Correcte gym via class_type
+    gym { session.class_type.gym }
+
     status { :confirmed }
     used_credits { 1 }
+
+    trait :canceled do
+      status { :canceled }
+      canceled_at { Time.current }
+    end
+
+    trait :no_show do
+      no_show { true }
+    end
+
+    trait :late_cancel do
+      canceled
+      canceled_at { session.starts_at - 1.hour }
+    end
   end
 end
