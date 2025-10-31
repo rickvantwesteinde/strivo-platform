70b1d550302e2b5d774c360b6026ec136d7fd9c9
rickvantwesteinde <rick@vantwestein.de>
2025-10-31 19:39:54 +0100

    fix: use associations in session factory to avoid NULL constraints

--- spec/factories/sessions.rb
+++ spec/factories/sessions.rb
@@ -2,8 +2,8 @@
 
 FactoryBot.define do
   factory :session do
-    class_type
-    trainer
+    class_type { association :class_type }
+    trainer { association :trainer }
     starts_at { 1.hour.from_now }
     duration_minutes { 60 }
     capacity { 14 }
