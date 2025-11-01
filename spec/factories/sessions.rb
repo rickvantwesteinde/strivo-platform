83cab88129f64ceda5438a99b8483cfa71a5abf7
rickvantwesteinde <rick@vantwestein.de>
2025-11-01 16:28:55 +0100

    Update sessions.rb

--- spec/factories/sessions.rb
+++ spec/factories/sessions.rb
@@ -4,8 +4,8 @@ FactoryBot.define do
   factory :session do
     class_type { association :class_type }
     trainer    { association :trainer }
-    start_at   { 1.hour.from_now }  # ← wijziging hier
+    start_at   { 1.hour.from_now }   # <-- was starts_at
     duration_minutes { 60 }
-    capacity { 14 }                 # mag blijven of toevoegen
+    capacity { 14 }
   end
 end
