f0a7974d01c354f503da9bf057c1c8543c4ef714
rickvantwesteinde <rick@vantwestein.de>
2025-11-01 22:47:59 +0100

    Update shoulda_matchers.rb

--- spec/support/shoulda_matchers.rb
+++ spec/support/shoulda_matchers.rb
@@ -1,8 +1,31 @@
 # frozen_string_literal: true
 
-Shoulda::Matchers.configure do |config|
-  config.integrate do |with|
-    with.test_framework :rspec
-    with.library :rails
+# -----------------------------------------------------------------------------
+# Shoulda Matchers configuratie met veilige fallback.
+#
+# Doel:
+#   - Integreert Shoulda Matchers met RSpec + Rails
+#   - Crasht niet als de gem ontbreekt (bijv. in CI of bij minimale test setups)
+#
+# Werking:
+#   - Probeert de gem te laden
+#   - Als dat lukt → standaard integratie
+#   - Als dat mislukt → toont waarschuwing en gaat verder zonder shoulda
+#
+# -----------------------------------------------------------------------------
+
+begin
+  require "shoulda-matchers"
+
+  Shoulda::Matchers.configure do |config|
+    config.integrate do |with|
+      with.test_framework :rspec
+      with.library :rails
+    end
   end
+
+  puts "[INFO] Shoulda Matchers succesvol geïntegreerd."
+rescue LoadError, NameError => e
+  warn "[WARN] Shoulda Matchers niet geladen (#{e.class}: #{e.message}). " \
+       "RSpec draait door zonder Shoulda-matchers."
 end
