10ab76c3073079707cdc8be24764a7059bee9c2e
rickvantwesteinde <rick@vantwestein.de>
2025-11-01 16:17:32 +0100

    Update sessions.rb

--- spec/factories/sessions.rb
+++ spec/factories/sessions.rb
@@ -1,8 +1,11 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :session do
     class_type { association :class_type }
-    trainer { association :trainer }
-    starts_at { 1.hour.from_now }
+    trainer    { association :trainer }
+    start_at   { 1.hour.from_now }  # ← wijziging hier
     duration_minutes { 60 }
+    capacity { 14 }                 # mag blijven of toevoegen
   end
 end
