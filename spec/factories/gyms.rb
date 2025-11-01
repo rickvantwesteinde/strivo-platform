43d147e50638ae8eb09a309b0306bf2691bb99ed
rickvantwesteinde <rick@vantwestein.de>
2025-11-01 20:04:52 +0100

    Update gyms.rb

--- spec/factories/gyms.rb
+++ spec/factories/gyms.rb
@@ -1,13 +1,9 @@
-# frozen_string_literal: true 
-# spec/factories/gyms.rb
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :gym do
     sequence(:name) { |n| "Gym #{n}" }
     sequence(:slug) { |n| "gym-#{n}" }
-  end
-
-  factory :default_gym, class: "Gym" do
-    name { "Default Gym" }
-    slug { "default-gym" }
+    address { "Amsterdam" }
   end
 end
