7866dbd882ef647aaf849d938197f5ce4512143e
rickvantwesteinde <rick@vantwestein.de>
2025-11-01 23:28:50 +0100

    Refactor rails_helper.rb for improved clarity

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,36 +1,30 @@
-# spec/rails_helper.rb
 # frozen_string_literal: true
 
-ENV['RAILS_ENV'] ||= 'test'
+ENV["RAILS_ENV"] ||= "test"
+require File.expand_path("../config/environment", __dir__)
+abort("The Rails environment is running in production mode!") if Rails.env.production?
 
-require 'spec_helper'
-require File.expand_path('../config/environment', __dir__)
-abort('The Rails environment is running in production mode!') if Rails.env.production?
+require "rspec/rails"
 
-require 'rspec/rails'
+# Laad alles in spec/support (matchers, helpers, etc.)
+Dir[Rails.root.join("spec/support/**/*.rb")].sort.each { |f| require f }
 
-# Houd de testdatabase synchroon met migraties
-begin
-  ActiveRecord::Migration.maintain_test_schema!
-rescue ActiveRecord::PendingMigrationError => e
-  puts e.to_s.strip
-  exit 1
-end
-
-# Laad alle support helpers (incl. shoulda, capybara, factory_bot, etc.)
-Dir[Rails.root.join('spec/support/**/*.rb')].sort.each { |f| require f }
+# Houd schema in sync
+ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
-  # FactoryBot short-hands
-  config.include FactoryBot::Syntax::Methods
-
-  # Transacties per test (snel & schoon)
   config.use_transactional_fixtures = true
-
-  # Spec type afleiden uit pad (model/controller/feature/system)
   config.infer_spec_type_from_file_location!
-
   config.filter_rails_from_backtrace!
+
+  # DatabaseCleaner (alleen als je 'm gebruikt)
+  if defined?(DatabaseCleaner)
+    config.before(:suite) do
+      DatabaseCleaner.strategy = :transaction
+      DatabaseCleaner.clean_with(:truncation)
+    end
+    config.around(:each) do |example|
+      DatabaseCleaner.cleaning { example.run }
+    end
+  end
 end
