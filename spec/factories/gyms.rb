e8549e266971ece4c1fda554f6fdaeb3431f4e5b
rickvantwesteinde <rick@vantwestein.de>
2025-11-01 14:53:24 +0100

    Add default_gym factory to gyms.rb

--- spec/factories/gyms.rb
+++ spec/factories/gyms.rb
@@ -1,6 +1,12 @@
+# spec/factories/gyms.rb
 FactoryBot.define do
   factory :gym do
     sequence(:name) { |n| "Gym #{n}" }
     sequence(:slug) { |n| "gym-#{n}" }
+  end
+
+  factory :default_gym, class: "Gym" do
+    name { "Default Gym" }
+    slug { "default-gym" }
   end
 end
