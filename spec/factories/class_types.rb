0dd8c5030166ed2d834503bb6eade6d8c0a566d6
rickvantwesteinde <rick@vantwestein.de>
2025-11-01 20:05:34 +0100

    Update class_types.rb

--- spec/factories/class_types.rb
+++ spec/factories/class_types.rb
@@ -1,7 +1,12 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :class_type do
-    gym
+    association :gym
     sequence(:name) { |n| "Class #{n}" }
+    description { "Workout" }
+    default_capacity { 14 }
     default_duration_minutes { 60 }
+    default_cancellation_cutoff_hours { 6 }
   end
 end
