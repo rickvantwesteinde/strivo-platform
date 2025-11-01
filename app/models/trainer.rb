ae705546eab616dc74b11bf440cfff1fa36e3b07
rickvantwesteinde <rick@vantwestein.de>
2025-11-01 20:03:20 +0100

    Update trainer.rb

--- app/models/trainer.rb
+++ app/models/trainer.rb
@@ -1,5 +1,11 @@
+# frozen_string_literal: true
+
 class Trainer < ApplicationRecord
   belongs_to :gym
-  belongs_to :user, foreign_key: :user_id, class_name: 'Spree::User'
-  has_many :sessions, dependent: :destroy
+  belongs_to :user, class_name: "Spree::User"
+
+  has_many :sessions, dependent: :nullify
+
+  validates :gym, :user, presence: true
+  validates :user_id, uniqueness: { scope: :gym_id }
 end
