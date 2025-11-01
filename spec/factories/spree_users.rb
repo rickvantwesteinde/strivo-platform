06c02bd313eb1ea5b631cb30c6c8b8dac806c406
rickvantwesteinde <rick@vantwestein.de>
2025-11-01 20:05:10 +0100

    Update spree_users.rb

--- spec/factories/spree_users.rb
+++ spec/factories/spree_users.rb
@@ -1,7 +1,10 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
-  factory :spree_user, class: 'Spree::User' do
+  factory :spree_user, class: "Spree::User" do
     sequence(:email) { |n| "user#{n}@example.com" }
-    password { 'Password1!' }
-    password_confirmation { 'Password1!' }
+    password { "Password1!" }
+    password_confirmation { "Password1!" }
+    confirmed_at { Time.current if Spree::User.column_names.include?("confirmed_at") }
   end
 end
