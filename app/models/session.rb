c38280767048ddfcec7e3e400b14721e8523b359
rickvantwesteinde <rick@vantwestein.de>
2025-11-01 17:42:20 +0100

    Update session.rb

--- app/models/session.rb
+++ app/models/session.rb
@@ -1,5 +1,9 @@
+# app/models/session.rb
 class Session < ApplicationRecord
+  belongs_to :gym
   belongs_to :class_type
   belongs_to :trainer
-  delegate :gym, to: :class_type
+
+  # (optioneel) validaties
+  validates :starts_at, presence: true
 end
