d8eb6d264d59aff2393fa76942f9c03db01144e9
rickvantwesteinde <rick@vantwestein.de>
2025-11-01 23:01:45 +0100

    Add Capybara configuration for headless Chrome

---
+++ spec/support/capybara.rb
@@ -0,0 +1,21 @@
+# spec/support/capybara.rb
+# frozen_string_literal: true
+
+require 'capybara/rspec'
+require 'selenium-webdriver'
+
+Capybara.default_max_wait_time = 5
+Capybara.server = :puma, { Silent: true }
+
+# Headless Chrome
+Capybara.register_driver :headless_chrome do |app|
+  opts = Selenium::WebDriver::Chrome::Options.new
+  opts.add_argument('--headless=new')
+  opts.add_argument('--no-sandbox')
+  opts.add_argument('--disable-gpu')
+  opts.add_argument('--window-size=1400,1400')
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: opts)
+end
+
+Capybara.javascript_driver = :headless_chrome
+Capybara.default_driver   = :rack_test
