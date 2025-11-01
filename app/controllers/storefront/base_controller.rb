66699a7857b4ca4131442a7109606d18a8268536
rickvantwesteinde <rick@vantwestein.de>
2025-11-01 14:33:25 +0100

    Refactor login guard and current gym retrieval

--- app/controllers/storefront/base_controller.rb
+++ app/controllers/storefront/base_controller.rb
@@ -4,17 +4,25 @@ module Storefront
   class BaseController < ApplicationController
     include Devise::Controllers::Helpers
     include Spree::Core::ControllerHelpers::Auth
-    helper Storefront::CreditsHelper
+    include Storefront::CreditsHelper
 
     before_action :require_spree_login
     before_action :set_current_gym
 
+    helper Storefront::CreditsHelper
     helper_method :current_gym, :current_spree_user
 
     private
 
+    # Spree standaard login guard
+    def require_spree_login
+      unless spree_current_user
+        redirect_to spree_login_path
+      end
+    end
+
     def set_current_gym
-      @current_gym = current_spree_user.gyms.first || Gym.first
+      @current_gym = spree_current_user&.gyms&.first || Gym.first
     end
 
     def current_gym
