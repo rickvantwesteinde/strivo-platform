0ab1d4704249b2a8cc7b417621c299a7d7db70cb
rickvantwesteinde <rick@vantwestein.de>
2025-11-01 20:06:48 +0100

    Update sessions.rb

--- spec/factories/sessions.rb
+++ spec/factories/sessions.rb
@@ -2,10 +2,17 @@
 
 FactoryBot.define do
   factory :session do
-    class_type { association :class_type }
-    trainer    { association :trainer }
-    start_at   { 1.hour.from_now }   # <-- was starts_at
+    association :class_type
+    # trainer en gym worden consistent met class_type gezet
+    starts_at { 2.days.from_now.change(hour: 10, min: 0) }
     duration_minutes { 60 }
     capacity { 14 }
+    cancellation_cutoff_hours { 6 }
+
+    # Koppel trainer én gym op dezelfde gym als class_type
+    after(:build) do |session|
+      session.gym ||= session.class_type&.gym || build(:gym)
+      session.trainer ||= build(:trainer, gym: session.gym)
+    end
   end
 end
