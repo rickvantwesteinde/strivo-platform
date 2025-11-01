4006b510251008a9b6498ce283fc6be22a3e7707
rickvantwesteinde <rick@vantwestein.de>
2025-11-01 23:29:07 +0100

    Handle LoadError for shoulda-matchers
    Add error handling for missing shoulda-matchers gem

--- spec/support/shoulda_matchers.rb
+++ spec/support/shoulda_matchers.rb
@@ -1,11 +1,15 @@
-# spec/support/shoulda_matchers.rb
 # frozen_string_literal: true
 
-require 'shoulda/matchers'
+# Zorg dat de gem bestaat; anders geen hard crash tijdens load
+begin
+  require "shoulda/matchers"
 
-Shoulda::Matchers.configure do |config|
-  config.integrate do |with|
-    with.test_framework :rspec
-    with.library :rails
+  Shoulda::Matchers.configure do |config|
+    config.integrate do |with|
+      with.test_framework :rspec
+      with.library :rails
+    end
   end
+rescue LoadError
+  warn "[WARN] shoulda-matchers not loaded (gem missing)."
 end
