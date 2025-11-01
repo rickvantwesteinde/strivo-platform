8500cee34abd055d88710202cb9dc9a221a856c3
rickvantwesteinde <rick@vantwestein.de>
2025-11-01 23:42:21 +0100

    Replace spree login with user authentication

--- app/controllers/storefront/base_controller.rb
+++ app/controllers/storefront/base_controller.rb
@@ -6,29 +6,15 @@ module Storefront
     include Spree::Core::ControllerHelpers::Auth
     include Storefront::CreditsHelper
 
-    before_action :require_spree_login
-    before_action :set_current_gym
+    before_action :authenticate_user!
 
     helper Storefront::CreditsHelper
-    helper_method :current_gym, :default_gym, :spree_current_user
+    helper_method :current_gym
 
     private
 
-    def require_spree_login
-      redirect_to spree_login_path unless spree_current_user
-    end
-
-    # Single-gym setup (later kun je dit vervangen door user->gyms)
-    def set_current_gym
-      @current_gym = default_gym
-    end
-
     def current_gym
-      @current_gym || default_gym
-    end
-
-    def default_gym
-      @default_gym ||= Gym.first
+      @current_gym ||= spree_current_user&.gyms&.first || Gym.first
     end
   end
 end
