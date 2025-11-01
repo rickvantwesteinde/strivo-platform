48ac38803b5acc6156dbc3ac4e478bdb68a133cb
rickvantwesteinde <rick@vantwestein.de>
2025-11-01 19:25:43 +0100

    Update 20250731104000_create_sessions.rb

--- db/migrate/20250731104000_create_sessions.rb
+++ db/migrate/20250731104000_create_sessions.rb
@@ -6,18 +6,13 @@
 
 class CreateSessions < ActiveRecord::Migration[8.0]
   def change
-    # Als :sessions niet bestaat, niets doen; CoreDomain maakt 'm aan.
     unless table_exists?(:sessions)
       say "Skipping legacy CreateSessions; sessions will be created by CreateCoreDomain", true
       return
     end
 
-    # Alleen veilige index bijzetten op bestaande kolommen.
-    if column_exists?(:sessions, :class_type_id) && column_exists?(:sessions, :starts_at)
-      add_index :sessions,
-                %i[class_type_id starts_at],
-                name: "index_sessions_on_class_type_id_and_starts_at"
-                unless index_exists?(:sessions, %i[class_type_id starts_at], name: "index_sessions_on_class_type_id_and_starts_at")
-    end
+    add_index :sessions, %i[class_type_id starts_at] \
+      if column_exists?(:sessions, :class_type_id) &&
+         !index_exists?(:sessions, %i[class_type_id starts_at])
   end
 end
