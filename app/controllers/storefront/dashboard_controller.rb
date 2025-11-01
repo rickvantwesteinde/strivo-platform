3dd9d823643f8e60cdfa8c29ccd8c784c26e8219
rickvantwesteinde <rick@vantwestein.de>
2025-11-01 16:42:44 +0100

    Update dashboard_controller.rb

--- app/controllers/storefront/dashboard_controller.rb
+++ app/controllers/storefront/dashboard_controller.rb
@@ -3,13 +3,14 @@
 module Storefront
   class DashboardController < Storefront::BaseController
     def show
+      # Credits voor huidige gebruiker
       @credits = CreditLedger.where(user: spree_current_user, gym: current_gym).sum(:amount)
 
-      @sessions = Session
-                    .where("start_at >= ?", Time.current) # <-- start_at
-                    .order(:start_at)
-                    .limit(10)
-                    .includes(:class_type, trainer: :user)
+      # SESSIES VAN VANDAAG
+      @sessions_today = Session
+        .where(start_at: Time.current.beginning_of_day..Time.current.end_of_day)
+        .order(:start_at)
+        .includes(:class_type, trainer: :user)
     end
   end
 end
