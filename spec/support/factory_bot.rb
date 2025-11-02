839087623a04e75d46ca82f5873d1df443edaa02
rickvantwesteinde <rick@vantwestein.de>
2025-11-02 01:55:04 +0100

    Enhance factory_bot.rb with usage comments
    Added comments to improve clarity on FactoryBot usage.

--- spec/support/factory_bot.rb
+++ spec/support/factory_bot.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 
 RSpec.configure do |config|
+  # Zorg voor mooie, korte helpers: build(:user) i.p.v. FactoryBot.build(:user)
   config.include FactoryBot::Syntax::Methods
+
+  # In Rails laadt factory_bot_rails automatisch je factories uit spec/factories.
+  # Extra defensieve laadactie kan, maar is niet vereist:
+  # config.before(:suite) { FactoryBot.find_definitions }
 end
