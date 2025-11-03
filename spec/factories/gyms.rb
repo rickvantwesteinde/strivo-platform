9bec6ddf701e0c7fe02e087bef9c87e09854b0bf
rickvantwesteinde <rick@vantwestein.de>
2025-11-03 18:56:23 +0100

    Revert "Add default_gym factory to gyms.rb"
    This reverts commit e8549e266971ece4c1fda554f6fdaeb3431f4e5b.

--- spec/factories/gyms.rb
+++ spec/factories/gyms.rb
@@ -1,12 +1,6 @@
-# spec/factories/gyms.rb
 FactoryBot.define do
   factory :gym do
     sequence(:name) { |n| "Gym #{n}" }
     sequence(:slug) { |n| "gym-#{n}" }
-  end
-
-  factory :default_gym, class: "Gym" do
-    name { "Default Gym" }
-    slug { "default-gym" }
   end
 end
