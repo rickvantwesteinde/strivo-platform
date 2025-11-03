2bccda6ca24e337386ea74166fa243b6d6524035
rickvantwesteinde <rick@vantwestein.de>
2025-11-03 11:04:35 +0100

    Refactor home controller specs for clarity

--- spec/controllers/spree/home_controller_spec.rb
+++ spec/controllers/spree/home_controller_spec.rb
@@ -1,15 +1,23 @@
+# spec/controllers/spree/home_controller_spec.rb
 require 'rails_helper'
 
 RSpec.describe Spree::HomeController, type: :controller do
-  describe 'GET #index' do
-    subject { get :index }
+  # Maak Spree engine-routes beschikbaar voor deze spec.
+  # Gebruik Frontend als je spree_frontend als aparte engine hebt,
+  # anders Core (werkt in veel setups).
+  routes { Spree::Core::Engine.routes }
+  # Alternatief (als je Frontend-namespace gebruikt):
+  # routes { Spree::Frontend::Engine.routes }
 
+  describe 'GET #index' do
     it 'returns a successful response' do
-      expect(subject).to be_successful
+      get :index
+      expect(response).to be_successful
     end
 
     it 'renders the index template' do
-      expect(subject).to render_template(:index)
+      get :index
+      expect(response).to render_template(:index)
     end
   end
 end
