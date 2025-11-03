3e9dd31feb4798f4e9a2198116786406d3723e12
rickvantwesteinde <rick@vantwestein.de>
2025-11-03 18:56:26 +0100

    Revert "Add default gym setup for RSpec tests"
    This reverts commit 97c545074e8e002a2aa9549985d2b78d49f133ca.

--- ensure_default_gym.rb
+++
@@ -1,8 +0,0 @@
-# Zorgt dat er altijd precies 1 gym bestaat tijdens specs
-RSpec.configure do |config|
-  config.before(:suite) do
-    Gym.find_or_create_by!(slug: "default-gym") do |g|
-      g.name = "Default Gym"
-    end
-  end
-end
