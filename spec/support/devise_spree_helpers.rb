e81ac59546877dce08cc81d305e4f4fc7685041a
rickvantwesteinde <rick@vantwestein.de>
2025-11-04 15:13:53 +0100

    Fix authentication helpers for request specs (#36)

--- spec/support/devise_spree_helpers.rb
+++ spec/support/devise_spree_helpers.rb
@@ -1,6 +1,12 @@
 module DeviseSpreeHelpers
   def sign_in_spree(user)
-    sign_in user, scope: :spree_user
+    login_as(user, scope: :spree_user)
+    sign_in(user, scope: :spree_user) if respond_to?(:sign_in)
+  end
+
+  def sign_in_spree_admin(admin)
+    login_as(admin, scope: :spree_admin_user)
+    sign_in(admin, scope: :spree_admin_user) if respond_to?(:sign_in)
   end
 end
 
